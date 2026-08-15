d561f0bef9e455b2ceed6358197cff3fff2455cd
Lewis <vicklewis35@gmail.com>
2026-08-15 23:22:24 +0100

    theme button works aand persists over refreshes

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,9 +1,18 @@
-import "../assets/stylesheets/index.css"
+import "../assets/stylesheets/index.css";
+
+export function bindThemeButton() {
+  if (!themeButton) { return; }
+
+  themeButton.addEventListener("click", function () {
+    document.body.classList.toggle("light-mode");
+    localStorage.setItem("theme", localStorage.getItem("theme") === "light-mode" ? "dark-mode" : "light-mode");
+  })
+}
 
 export function ThemeButton() {
   return (
     <button id="themeButton" class="theme-toggle" type="button" aria-label="Toggle Theme">
       <span id="themeButtonIcon">☀︎</span>
     </button>
-  )
-}
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,17 @@
 import { render } from 'preact'
 import { App } from './app.jsx'
 
+import { bindThemeButton } from "./components/ThemeButton.jsx"
+
+function syncTheme() {
+  if (localStorage.getItem("theme") === "light-mode") {
+    // toggle light mode on
+    document.body.classList.add("light-mode");
+  }
+}
+
+
 render(<App />, document.getElementById('app'))
+
+syncTheme();
+bindThemeButton();
